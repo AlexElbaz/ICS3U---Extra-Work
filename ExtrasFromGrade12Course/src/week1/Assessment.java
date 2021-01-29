b9e734cfb61eea3005fdc58333d31f8191bf2aa9
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-01-29 16:30:08 -0500

    NOT WORKING - Student Gradebook Work, using arrays, variables, and multiple classes. Get Mr. DesLauriers from GitHub

---
+++ ExtrasFromGrade12Course/src/week1/Assessment.java
@@ -0,0 +1,35 @@
+package week1;
+
+public class Assessment {
+    private String name;
+    private int maxScore;
+    private int studentScore;
+
+    public Assessment(String name, int maxScore) {
+        this.name = name;
+        this.maxScore = maxScore;
+        this.studentScore = 0;
+    }
+
+    public Assessment(String name, int maxScore, int studentScore) {
+        this.name = name;
+        this.maxScore = maxScore;
+        this.studentScore = studentScore;
+    }
+
+    public double getAverage() {
+        return studentScore / (double)maxScore * 100;
+    }
+
+    public String toString() {
+        String temp = name + ": " + getAverage();
+    }
+
+    public int getScore() {
+        return studentScore;
+    }
+
+    public int getMaxScore() {
+        return maxScore;
+    }
+}
