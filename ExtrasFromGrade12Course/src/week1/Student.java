872086c9647941860aafdf68c80b5c0b0e4d6973
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-02-22 09:52:48 -0500

    Beginnings of a Student Gradebook (Now Working!).

--- ExtrasFromGrade12Course/src/week1/Student.java
+++ ExtrasFromGrade12Course/src/week1/Student.java
@@ -1,48 +1,48 @@
 package week1;
 
 public class Student {
-    private String firstName;
-    private String lastName;
-    private Assessment[]assessments;   // a partially filled array
-    private int numAssessmentsCompleted;
+  private String firstName;
+  private String lastName;
+  private Assessment[] assessments; // a partially filled array
+  private int numAssessmentsCompleted;
 
-    public Student(String fName, String lName) {
-        firstName = fName;
-        lastName = lName;
-        assessments = new Assessment[Course.NUM_ASSESSMENTS];
+  public Student(String fName, String lName){
+    firstName = fName;
+    lastName = lName;
+    assessments = new Assessment[Course.NUM_ASSESSMENTS];
+    numAssessmentsCompleted = 0;
+  }
+
+  public String getName(){
+    return lastName + ", " + firstName;
+  }
+
+  public void addAssessment(Assessment assessment){
+    assessments[numAssessmentsCompleted] = assessment;
+    numAssessmentsCompleted++;
+  }
+
+  public double getAverage(){
+    if (numAssessmentsCompleted == 0)
+      return 0;
+
+    double totalMarks = 0;
+    double totalMaxScore = 0;
+
+
+    for (int i=0; i < numAssessmentsCompleted; i++){
+      totalMarks += assessments[i].getScore();
+      totalMaxScore += assessments[i].getMaxScore();
     }
 
-    public String getName() {
-        return lastName + ", " + firstName;
+    return totalMarks / totalMaxScore;
+  }
+
+
+  public void displayStudentResults(){
+    for (int i=0; i < numAssessmentsCompleted; i++){
+      System.out.println(assessments[i]);
     }
+  }
 
-    public void addAssessment(Assessment assessment) {
-        assessments[numAssessmentsCompleted] = assessment;
-        numAssessmentsCompleted++;
-    }
-
-    public double getAverage() {
-        if (numAssessmentsCompleted == 0)
-            return 0;
-        
-        
-        double totalMarks = 0;
-        double totalMaxScore = 0;
-
-        for (int i=0; i < numAssessmentsCompleted; i++){
-            totalMarks += assessments[i].getScore();
-            totalMaxScore += assessments[i].getMaxScore();
-        }
-
-        return totalMarks / totalMaxScore;
-    }
-
-    public void displayStudentResult() {
-        for (int i=0; i < numAssessmentsCompleted; i++){
-            System.out.println(students[i].getName());
-            System.out.println("------------");
-            students[i].displayStudentResult();
-            System.out.println();
-        }
-    }
 }
